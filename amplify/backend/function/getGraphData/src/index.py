11cc1d0489f308930c65a39528cf771776ef4590
Nicole T <nbtrieu@uci.edu>
2024-04-03 14:38:50 -0700

    Testing getGraphData-dev

--- amplify/backend/function/getGraphData/src/index.py
+++ amplify/backend/function/getGraphData/src/index.py
@@ -7,23 +7,25 @@ from gremlin_python.process.anonymous_traversal import traversal
 
 
 def handler(event, context):
-    print('received event:')
-    print(event)
+    try:
+        print('received event:')
+        print(event)
 
-    gremlin_url = "wss://db-bio-annotations.cluster-cu9wyuyqqen8.ap-southeast-1.neptune.amazonaws.com:8182/gremlin"
-    print(gremlin_url)
-    # ssl_context = ssl.create_default_context(cafile=certifi.where())
-    # gremlin_client = DriverRemoteConnection(gremlin_url, "g", ssl_context=ssl_context)
-    # g = traversal().withRemote(gremlin_client)
-    # node_count = g.V().count().next()
-    # return node_count
+        gremlin_url = "wss://db-bio-annotations.cluster-cu9wyuyqqen8.ap-southeast-1.neptune.amazonaws.com:8182/gremlin"
+        print(gremlin_url)
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        'body': json.dumps('Hello from your new Amplify Python lambda!')
-    }
+        # ssl_context = ssl.create_default_context(cafile=certifi.where())
+        # gremlin_client = DriverRemoteConnection(gremlin_url, "g", ssl_context=ssl_context)
+        # g = traversal().withRemote(gremlin_client)
+        # node_count = g.V().count().next()
+
+        # return {
+        #     'statusCode': 200,
+        #     'body': json.dumps({'message': 'Successfully retrieved node count', 'node_count': node_count})
+        # }
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'An error occurred while processing your request.'})
+        }
