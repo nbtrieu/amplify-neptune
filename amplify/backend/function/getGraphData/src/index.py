a13c931e203ba0a363056afb8cc56f11bb1205b9
Nicole T <nbtrieu@uci.edu>
2024-04-04 16:02:45 -0700

    Testing gremlin query again

--- amplify/backend/function/getGraphData/src/index.py
+++ amplify/backend/function/getGraphData/src/index.py
@@ -8,69 +8,24 @@ from gremlin_python.process.anonymous_traversal import traversal
 
 
 def handler(event, context):
-    # try:
-    #     gremlin_url = "wss://db-bio-annotations.cluster-cu9wyuyqqen8.ap-southeast-1.neptune.amazonaws.com:8182/gremlin"
-    #     # print("GREMLIN URL", gremlin_url)
-
-    #     ssl_context = ssl.create_default_context(cafile=certifi.where())
-    #     # print(ssl_context)
-    #     gremlin_client = DriverRemoteConnection(gremlin_url, "g", ssl_context=ssl_context)
-    #     # print(gremlin_client)
-    #     g = traversal().withRemote(gremlin_client)
-    #     node_count = g.V().count().next()
-
-    #     return {
-    #         'statusCode': 200,
-    #         'body': json.dumps({'message': 'Successfully retrieved node count', 'node_count': node_count})
-    #     }
-    # except Exception as e:
-    #     print(f"An error occurred: {str(e)}")
-    #     return {
-    #         'statusCode': 500,
-    #         'body': json.dumps({'error': 'An error occurred while processing your request.'})
-    #     }
-
     try:
-        data = {'Name': ['John', 'Anna', 'Peter', 'Linda'],
-                'Age': [28, 34, 29, 32]}
-        df = pd.DataFrame(data)
+        gremlin_url = "wss://db-bio-annotations.cluster-cu9wyuyqqen8.ap-southeast-1.neptune.amazonaws.com:8182/gremlin"
+        # print("GREMLIN URL", gremlin_url)
 
-        result = df.to_json(orient='records')
+        ssl_context = ssl.create_default_context(cafile=certifi.where())
+        # print(ssl_context)
+        gremlin_client = DriverRemoteConnection(gremlin_url, "g", ssl_context=ssl_context)
+        # print(gremlin_client)
+        g = traversal().withRemote(gremlin_client)
+        node_count = g.V().count().next()
 
         return {
             'statusCode': 200,
-            'body': json.dumps({'message': 'DataFrame created successfully', 'data': result})
+            'body': json.dumps({'message': 'Successfully retrieved node count', 'node_count': node_count})
         }
     except Exception as e:
+        print(f"An error occurred: {str(e)}")
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': f'An error occurred: {str(e)}'})
+            'body': json.dumps({'error': 'An error occurred while processing your request.'})
         }
-
-
-# import http.client
-
-
-# def handler(event, context):
-#     try:
-#         conn = http.client.HTTPSConnection("www.example.com")
-#         conn.request("GET", "/")
-#         response = conn.getresponse()
-
-#         if response.status == 200:
-#             print("HTTPS request succeeded")
-#         else:
-#             print(f"HTTPS request failed with status: {response.status}")
-
-#         conn.close()
-
-#         return {
-#             'statusCode': 200,
-#             'body': 'HTTPS request test completed'
-#         }
-#     except Exception as e:
-#         print(f"An error occurred: {str(e)}")
-#         return {
-#             'statusCode': 500,
-#             'body': 'HTTPS request test failed'
-#         }
