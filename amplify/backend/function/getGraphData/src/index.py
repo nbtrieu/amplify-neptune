cb408e47a118d8522658b1084718e1e982f5858a
Nicole T <nbtrieu@uci.edu>
2024-04-04 10:20:31 -0700

    Fix API endpoint

--- amplify/backend/function/getGraphData/src/index.py
+++ amplify/backend/function/getGraphData/src/index.py
@@ -38,7 +38,7 @@ import aiohttp
 
 async def test_aiohttp():
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.example.com/endpoint') as response:
+        async with session.get('https://catfact.ninja/fact') as response:
             if response.status == 200:
                 data = await response.json()
                 print(data)
