6e8b071590affc8e595f8ecb95c3b6f1c45a4804
Nicole T <nbtrieu@uci.edu>
2024-04-09 11:59:17 -0700

    Modify function to check a specific node's properties

--- amplify/backend/function/getGraphData/src/index.py
+++ amplify/backend/function/getGraphData/src/index.py
@@ -1,8 +1,6 @@
 import json
 import ssl
 import certifi
-# import aiohttp
-# import pandas as pd
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from gremlin_python.process.anonymous_traversal import traversal
 
@@ -13,20 +11,16 @@ def handler(event, context):
         print("GREMLIN URL", gremlin_url)
 
         ssl_context = ssl.create_default_context(cafile=certifi.where())
-        # print(ssl_context)
         gremlin_client = DriverRemoteConnection(
             gremlin_url, "g", ssl_context=ssl_context
         )
-        # print(gremlin_client)
         g = traversal().withRemote(gremlin_client)
         node_count = g.V().count().next()
-        print(f"Successfully retrieved node count: {node_count}")
 
-        # return {
-        #     'statusCode': 200,
-        #     'body': json.dumps({'message': 'Successfully retrieved node count', 'node_count': node_count})
-        # }
-        return g
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Successfully retrieved node count', 'node_count': node_count})
+        }
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
