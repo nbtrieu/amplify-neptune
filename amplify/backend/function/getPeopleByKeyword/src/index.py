3e1c6332d655d7b8f9fd1ebebb33907437ef68bf
Nicole T. <nbtrieu@uci.edu>
2024-04-18 16:04:10 -0700

    Add Lambda function to query people for keywords

---
+++ amplify/backend/function/getPeopleByKeyword/src/index.py
@@ -0,0 +1,41 @@
+import json
+import ssl
+import certifi
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.anonymous_traversal import traversal
+from gremlin_python.process.graph_traversal import __
+
+
+def handler(event, context):
+    try:
+        keyword = event.get('keyword')
+
+        gremlin_url = "wss://db-bio-annotations.cluster-cu9wyuyqqen8.ap-southeast-1.neptune.amazonaws.com:8182/gremlin"
+        ssl_context = ssl.create_default_context(cafile=certifi.where())
+        gremlin_client = DriverRemoteConnection(
+            gremlin_url, "g", ssl_context=ssl_context
+        )
+        g = traversal().withRemote(gremlin_client)
+
+        result = (
+            g.V()
+            .has("keyword", "name", keyword)
+            .bothE()
+            .outV()
+            .hasLabel("person")
+            .valueMap(True)
+            .dedup()
+            .toList()
+        )
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(result)
+        }
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'An error occurred while processing your request.'})
+        }
