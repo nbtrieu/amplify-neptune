2ff373ee4805509aa0eeea122fc9506355e80bd8
Nicole T. <nbtrieu@uci.edu>
2024-05-07 16:07:09 -0700

    Add function to search by name value

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports.js';
