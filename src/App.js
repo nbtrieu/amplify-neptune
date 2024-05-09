c656ff079853582982ae531495f93dbcb451e04a
Nicole T. <nbtrieu@uci.edu>
2024-05-08 17:55:22 -0700

    Add front-end for user authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports.js';
 
@@ -8,17 +10,32 @@ import KeywordSearchPage from './pages/KeywordSearchPage.js';
 import OrganizationSearchPage from './pages/OrganizationSearchPage.js';
 import NameSearchPage from './pages/NameSearchPage.js';
 
+import '@aws-amplify/ui-react/styles.css';
+
 Amplify.configure(config);
 
-function App() {  
+function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<KeywordSearchPage />} />
-        <Route path='/organization' element={<OrganizationSearchPage />} />
-        <Route path='/name' element={<NameSearchPage />} />
-      </Routes>
+      <Authenticator>
+        {({ signOut, user }) => {
+          // Redirect user to the login page if not signed in
+          if (!user) return <Navigate to="/auth" replace />;
+          
+          // Return main app if user is signed in
+          return (
+            <>
+              <NavBar signOut={signOut} />
+              <Routes>
+                <Route path='/' element={<KeywordSearchPage />} />
+                <Route path='/organization' element={<OrganizationSearchPage />} />
+                <Route path='/name' element={<NameSearchPage />} />
+                <Route path='/auth' element={<></>} />
+              </Routes>
+            </>
+          );
+        }}
+      </Authenticator>
     </Router>
   );
 }
