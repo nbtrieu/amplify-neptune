48d5f9d3a6de5dc3915e5abda5d934822b6e5556
Nicole T. <nbtrieu@uci.edu>
2024-08-19 12:54:25 -0700

    Testing multiple query selection

--- src/pages/CombinedKeywordSearchPage.js
+++ src/pages/CombinedKeywordSearchPage.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DoubleSearchBox, SearchResultComponent, ResultsTable } from "../components";
 import { SearchProvider } from "../context/SearchContext";
-import { searchByKeyword } from "../graphql/queries.js";
+import { searchPersonByKeyword, /* other imports */ } from "../graphql/queries";
 import keywordOptionsList from "../options/keywordOptions.js";
 
 const CombinedKeywordSearchPage = () => {
+  const [selectedNodeType, setSelectedNodeType] = useState('person');
+  const [query, setQuery] = useState(searchPersonByKeyword);
+
   const nodeTypeOptions = [
     { value: 'person', label: 'people' },
     { value: 'organization', label: 'organizations' },
@@ -12,21 +15,36 @@ const CombinedKeywordSearchPage = () => {
     { value: 'zymo_product', label: 'Zymo products' },
     { value: 'publication_product', label: 'publication products' }
   ];
-	
-	return (
-		<SearchProvider>
-			<SearchResultComponent 
-				SearchBoxComponent={DoubleSearchBox}
-				searchProps={{
-					query: searchByKeyword, // to be replaced with the new lambda-linked graphql query
-					nodeTypeOptions: nodeTypeOptions,
-					keywordOptions: keywordOptionsList.map(keyword => ({ value: keyword, label: keyword })),
-          title: "Search any node types by keyword"
-				}}
-				ResultsTableComponent={ResultsTable}
-			/>
-		</SearchProvider>
-	)
+
+  // Map node types to their respective GraphQL queries
+  const queryMap = {
+    person: searchPersonByKeyword,
+    organization: searchPersonByKeyword,
+    publication: searchPersonByKeyword,
+    zymo_product: searchPersonByKeyword,
+    publication_product: searchPersonByKeyword
+  };
+
+  // Update the selected query based on the node type
+  useEffect(() => {
+    setQuery(queryMap[selectedNodeType]);
+  }, [selectedNodeType]);
+
+  return (
+    <SearchProvider>
+      <SearchResultComponent 
+        SearchBoxComponent={DoubleSearchBox}
+        searchProps={{
+          query: query, // Dynamically set query based on selected node type
+          nodeTypeOptions: nodeTypeOptions,
+          keywordOptions: keywordOptionsList.map(keyword => ({ value: keyword, label: keyword })),
+          title: "Search any node types by keyword",
+          onNodeTypeChange: setSelectedNodeType // Pass setter to DoubleSearchBox
+        }}
+        ResultsTableComponent={ResultsTable}
+      />
+    </SearchProvider>
+  );
 };
 
 export default CombinedKeywordSearchPage;
